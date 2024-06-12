df91e0ac70a558575cd094556c03a27a829b5a03
r88 <reflector88@gmail.com>
2024-06-12 10:30:44 -0700

    Open MGCB Editor

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-# MonoGame for VS Code
 
-Better VS Code integration for MonoGame. Adds features of the Visual Studio MonoGame extension to VS Code.
+**MonoGame for VSCode** simplifies the process of using MonoGame in Visual Studio Code by importing some of the features of the Visual Studio 2022 MonoGame extension.
 
-## Features
-- Create new projects from the command palette
-- Automatically install MonoGame framework templates
+- Automatically installs MonoGame templates
+- Easily create new projects
 - Double-click MGCB files to open the MGCB Editor
 
-For example if there is an image subfolder under your extension project workspace:
+<!-- \!\[feature X\]\(images/feature-x.png\)
 
-\!\[feature X\]\(images/feature-x.png\)
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow. -->
+## Quick Start
+To create your first MonoGame project in VS Code,
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1. Install the [.NET SDK](https://dotnet.microsoft.com/en-us/download) and [C# Dev Kit](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit). Restart VS Code, then install this extension.
+2. Open the command palette and choose **"MonoGame: Create New Project"**.
+3. Choose a project template (if you aren't sure, choose **"Cross-Platform Desktop Application"**).
+4. Enter project name and directory.
+
+Your new project will be created automatically at the directory and the window will refresh. The solution explorer (part of the C# Dev Kit) should appear in the sidebar, allowing you to build, run and debug your game.
+
+To open the MGCB editor, use the `MonoGame: Open MGCB Editor` command or click the MonoGame button in the tab bar.
+
+
 
 ## Requirements
 
+ - [.NET SDK](https://dotnet.microsoft.com/en-us/download)
  - [C# Dev Kit](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit)
 
 ## Extension Settings
@@ -25,26 +35,15 @@ For example:
 
 This extension contributes the following settings:
 
-* `MonoGamey'
+* `MonoGame`
 
 * `myExtension.enable`: Enable/disable this extension.
 * `myExtension.thing`: Set to `blah` to do something.
 
-## Known Issues
 
-None
 
 ## Release Notes
 
 ### 1.0.0
 
 Initial release
-
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
--- src/extension.ts
+++ src/extension.ts
@@ -1,87 +1,116 @@
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
 let myTerminal: vscode.Terminal;
 
-const quickPickOptions: vscode.QuickPickOptions = { placeHolder: "Select a Template" };
-
-const inputBoxOptions: vscode.InputBoxOptions = { placeHolder: "Enter Project Name" };
-
-const dialogOptions: vscode.OpenDialogOptions = {
-	openLabel: "Select Folder",
-	canSelectFiles: false,
-	canSelectFolders: true
-};
-
-const templates = new Map([
-	["$(device-desktop) Cross-Platform Desktop Application", "mgdesktopgl"],
-	["$(device-desktop) Windows Desktop Application", "mgwindowdx"],
-	["$(device-desktop) Windows Universal XAML Application", "mguwpxaml"],
-	["$(device-mobile) Android Application", "mgandroid"],
-	["$(device-mobile) iOS Application", "mgios"],
-	["$(tools) Content Pipeline Extension", "mgpipeline"],
-	["$(archive) Game Library", "mglib"],
-	["$(archive) Shared Library Project", "mgshared"],
-
-]);
-
 export function activate(context: vscode.ExtensionContext) {
 
-	//Creates new MonoGame project and solution at specified folder
-	const createProject = vscode.commands.registerCommand(
-		'monogame.createProject', () => {
-			getNewProjectInput();
-		});
-	context.subscriptions.push(createProject);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('monogame.createProject', () => {
+			getUserInput()
+		}),
 
-	const openMGCBEditor = vscode.commands.registerCommand(
-		'monogame.MGCB', () => {
-
-			let folderName: string = '';
-			if (vscode.workspace.workspaceFolders) {
-				const currentDir = vscode.workspace.workspaceFolders[0].uri.fsPath;
-				folderName = currentDir.slice(currentDir.lastIndexOf("\\") + 1);
-			}
-
-			runShellCommand("dotnet mgcb-editor ./Content/Content.mgcb");
-		});
-	context.subscriptions.push(openMGCBEditor);
-
+		vscode.commands.registerCommand('monogame.MGCB', () => {
+			openMGCBEditor();
+		})
+	);
 }
 
-async function getNewProjectInput() {
-	const template = await vscode.window.showQuickPick(Array.from(templates.keys()), quickPickOptions);
+async function install() {
+	vscode.window.showInformationMessage(
+		"Would you like to install MonoGame templates?", "Install", "Do Not Install")
+		.then((selection) => {
+			if (selection === "Install") {
+				runShellCommand("dotnet new install MonoGame.Templates.CSharp");
+				vscode.window.showInformationMessage("Installing...");
+			}
+		});
+}
+
+function openMGCBEditor() {
+
+	if (vscode.workspace.workspaceFolders) {
+		const workDir = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		const fileDir = `${workDir}\\Content\\Content.mgcb`;
+
+		child_process.execFile("dotnet", ["mgcb-editor", fileDir], { cwd: workDir });
+	}
+}
+
+// function isPackageInstalled(packageName: string): Promise<boolean> {
+// 	return new Promise((resolve, reject) => {
+// 		exec(`dotnet list package --name ${packageName}`, (error, stdout, stderr) => {
+// 			if (error) {
+// 				reject(error);
+// 				return;
+// 			}
+// 			resolve(stdout.includes(packageName));
+// 		});
+// 	});
+// }
+
+async function getUserInput() {
+	const templateCommands = new Map([
+		["$(device-desktop) Cross-Platform Desktop Application", "mgdesktopgl"],
+		["$(device-desktop) Windows Desktop Application", "mgwindowdx"],
+		["$(device-desktop) Windows Universal XAML Application", "mguwpxaml"],
+		["$(device-mobile) Android Application", "mgandroid"],
+		["$(device-mobile) iOS Application", "mgios"],
+		["$(tools) Content Pipeline Extension", "mgpipeline"],
+		["$(archive) Game Library", "mglib"],
+		["$(archive) Shared Library Project", "mgshared"],
+	]);
+
+	const template = await vscode.window.showQuickPick(Array.from(templateCommands.keys()), {
+		title: "Create MonoGame Project",
+		placeHolder: "Select a Template..."
+	});
 	if (!template) return;
 
-	const name = await vscode.window.showInputBox(inputBoxOptions);
+	const name = await vscode.window.showInputBox({
+		title: "Create MonoGame Project",
+		placeHolder: "Enter Project Name..."
+	});
 	if (!name) return;
 
-	const path = await vscode.window.showOpenDialog(dialogOptions);
+	const path = await vscode.window.showOpenDialog({
+		openLabel: "Select Folder",
+		canSelectFiles: false,
+		canSelectFolders: true
+	});
 	if (!path) return;
 
-	createNewProject(path, name, template);
+	createNewProject(path[0].fsPath, name.replaceAll(" ", "_"), templateCommands.get(template)!);
 }
 
-function createNewProject(path: vscode.Uri[], name: string, template: string) {
+function createNewProject(path: string, name: string, template: string) {
 	const commands = [
-		"cd " + path[0].fsPath,
-		"mkdir " + name,
-		"cd " + name,
-		"dotnet new " + templates.get(template) + " -n " + name,
-		"dotnet new sln -n " + name,
-		"dotnet sln add ./" + name + "/" + name + ".csproj",
-		"code ."
+		`cd ${path}`,
+		`mkdir ${name}`,
+		`cd ${name}`,
+		`dotnet new ${template} -n ${name}`,
+		`dotnet new sln -n ${name}`,
+		`dotnet sln add ./${name}/${name}.csproj`,
+		`cd ${name}`,
+		"code . -r"
 	];
 
 	for (const command of commands) runShellCommand(command);
-	vscode.window.showInformationMessage("\"" + name + "\" created at " + path[0].fsPath);
-
+	vscode.window.showInformationMessage("Creating new project...");
 }
+
 
 function runShellCommand(command: string) {
 	if (!myTerminal) {
-		myTerminal = vscode.window.createTerminal();
+		if (vscode.window.activeTerminal && vscode.window.activeTerminal.name === "powershell") {
+			myTerminal = vscode.window.activeTerminal;
+		} else {
+			myTerminal = vscode.window.createTerminal();
+		}
 	}
 	myTerminal.sendText(command);
 }
 
-export function deactivate() { }
+export function deactivate() {
+	myTerminal.dispose();
+}
